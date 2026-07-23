9715a29621a5407b8f2c3214a7988215803ad980
n-aadharshini <aadharshinidharshini@gmail.com>
2026-07-23 14:45:17 +0530

    gorq Api key added

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'core/routes/app_routes.dart';
 import 'core/theme/app_theme.dart';
 
-void main() => runApp(const VisionMateApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  print('Groq key loaded: ${dotenv.env['GROQ_API_KEY']?.substring(0, 8)}...');
+  runApp(const VisionMateApp());
+}
 
 class VisionMateApp extends StatelessWidget {
   const VisionMateApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
